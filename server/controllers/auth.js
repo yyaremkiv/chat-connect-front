a82185ee1b936c43ac3b6af0ef89379fe8224484
yyaremkiv <y.yaremkiv@gmail.com>
2023-03-23 23:19:21 +0200

    update

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -40,6 +40,7 @@ import User from "../models/User.js";
 
 export const login = async (req, res) => {
   try {
+    console.logсв;
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
 
